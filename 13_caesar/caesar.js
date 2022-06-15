a8e7774581241f7c048b0e7dd06af5e562a6a153
Justin Liu <44413809+JustACowSP@users.noreply.github.com>
2022-06-15 18:03:20 -0400

    Complete exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,22 @@
-const caesar = function() {
+const caesar = function (text, shift) {
+  let characterCodes = Array.from(text).map((char) => char.charCodeAt(0));
 
+  characterCodes = characterCodes.map((code) => {
+    let startingCharacter;
+    if (code >= 'A'.charCodeAt(0) && code <= 'Z'.charCodeAt(0)) {
+      startingCharacter = 'A';
+    } else if (code >= 'a'.charCodeAt(0) && code <= 'z'.charCodeAt(0)) {
+      startingCharacter = 'a';
+    } else {
+      return code;
+    }
+    let newCode = (((code - startingCharacter.charCodeAt(0)) + shift) % 26);
+    newCode = (newCode < 0) ? newCode + 26 : newCode;
+    return newCode + startingCharacter.charCodeAt(0);
+  });
+
+  return String.fromCharCode(...characterCodes);
+  // return characterCodes;
 };
 
 // Do not edit below this line
