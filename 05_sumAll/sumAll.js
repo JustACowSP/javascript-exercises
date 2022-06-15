910834c92f220270eba2c979785e548acceb8aca
Justin Liu <44413809+JustACowSP@users.noreply.github.com>
2022-06-15 14:00:56 -0400

    Complete exercise 05 and change linting style

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return 'ERROR';
+  } if (a < 0 || b < 0) {
+    return 'ERROR';
+  }
 
+  let begin;
+  let end;
+
+  if (a > b) {
+    [begin, end] = [b, a];
+  } else {
+    [begin, end] = [a, b];
+  }
+
+  let sum = 0;
+
+  for (let n = begin; n <= end; n += 1) {
+    sum += n;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
