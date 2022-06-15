315826a61981820824d10a60dba760d97a848a0b
Justin Liu <44413809+JustACowSP@users.noreply.github.com>
2022-06-15 16:44:59 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function fibonacci(k) {
+  if (k <= 0) {
+    return 'OOPS';
+  }
+  if (k === 1 || k === 2 || k === '1' || k === '2') {
+    return 1;
+  }
 
+  return fibonacci(k - 1) + fibonacci(k - 2);
 };
 
 // Do not edit below this line
