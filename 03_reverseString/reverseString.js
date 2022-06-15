f868a452973da882adbe29ef2923145f4b6908fb
Justin Liu <44413809+JustACowSP@users.noreply.github.com>
2022-06-15 13:01:36 -0400

    Complete exercises 01, 02, 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let backwards = '';
+    
+    for (let i = string.length - 1; i >= 0; i--) {
+        backwards += string[i];
+    }
 
+    return backwards;
 };
 
 // Do not edit below this line
