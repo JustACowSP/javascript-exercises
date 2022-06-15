cc974f2bf3e6af650ddb112fa1610b6bd0d26d77
Justin Liu <44413809+JustACowSP@users.noreply.github.com>
2022-06-15 13:21:31 -0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...valuesToRemove) {
+    let valuesToRemoveArray = [...valuesToRemove];
+    return array.filter(function (value) { return !valuesToRemoveArray.includes(value); });
 };
 
 // Do not edit below this line
