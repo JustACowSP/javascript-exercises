2f2c958f3b5b60f272380012d8e3fa18eac7c74c
Justin Liu <44413809+JustACowSP@users.noreply.github.com>
2022-06-15 15:01:40 -0400

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (fTemperature) {
+  return Math.round(((fTemperature - 32) * (5 / 9)) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (cTemperature) {
+  return Math.round((cTemperature * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
