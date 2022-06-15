7a61a6bbf89172610cca7dcadffe2572776c3a1c
Justin Liu <44413809+JustACowSP@users.noreply.github.com>
2022-06-15 17:22:38 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  return array.reduce((previousPerson, currentPerson) => {
+    const currentYear = new Date().getFullYear();
+    if ((currentPerson.yearOfDeath ?? currentYear) - currentPerson.yearOfBirth
+            > (previousPerson.yearOfDeath ?? currentYear) - previousPerson.yearOfBirth) {
+      return currentPerson;
+    }
+    return previousPerson;
+  });
 };
 
 // Do not edit below this line
