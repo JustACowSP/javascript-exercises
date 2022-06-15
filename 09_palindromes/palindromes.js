48caedce151b8c07e099810a8db5a920c113ab9d
Justin Liu <44413809+JustACowSP@users.noreply.github.com>
2022-06-15 16:25:40 -0400

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  const string = text.replace(/\W/g, '').toLowerCase();
 
+  for (let i = 0; i < string.length / 2; i += 1) {
+    if (string[i] !== string[string.length - i - 1]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
